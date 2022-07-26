ef29cbee404defcd937a4ae012338455aa822ba7
r saranya <109914916+A2bcs@users.noreply.github.com>
2022-07-26 20:57:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,10 +62,10 @@ if streamlit.button('get fruit load list'):
      streamlit.dataframe(my_data_rows)
 
 #allow the end user to add a fruit to the list
- def insert_row_snowflake(new_fruit)
-     with my_cnx.cursor() as my_cur:
-          my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-          return "Thanks for adding" + new_fruit
+  def insert_row_snowflake(new_fruit):
+      with my_cnx.cursor() as my_cur:
+           my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+           return "Thanks for adding" + new_fruit
     
 add_my_fruit= streamlit.text_input('What fruit would you like to add?','jackfruit')
 if streamlit.button('add a fruit to the list'):
