f940e4066950d0863e86d8567a7de4c464f85dd2
r saranya <109914916+A2bcs@users.noreply.github.com>
2022-07-26 19:30:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,6 +63,4 @@ my_cur.execute("insert into fruit_load_list values('from streamlit')")
 #dont run anything past here while we troubleshoot
 streamlit.stop()
 
-import snowflake.connector
-from urlib.error import URLError
 
